9bff0fa14326284b4bd0eb7e5d934961d65454a5
unknown <ycurrao@ARBUEL018.hq.inclusionservices.com>
2016-01-12 15:27:04 -0300

    Commit de Assertion que se agregan a las urls tanto para firefox como para chrome

--- src/chromePruebas/PruebaDeUrls.java
+++ src/chromePruebas/PruebaDeUrls.java
@@ -26,122 +26,203 @@ public class PruebaDeUrls {
 	}
 
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testVentas(){
+	public void testVentas() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Ventas***********/ pruebo todos sus enlaces:		
-		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/ve-ventas-consolidado");//Sancor
-		//driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/ve-ventas-consolidado");//Bonificaciones
-	    //driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/ve-ventas-consolidado");//Rofina
+		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/ve-ventas-consolidado?fdv=SANCOR_DW");//Sancor
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Ventas Sancor");
+		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/ve-ventas-consolidado?fdv=ROFINA");//Rofina
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Ventas Rofina");
+//		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/ve-ventas-consolidado");//Bonificaciones
+//			Thread.sleep(5000);
+//			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Ventas Bonificaciones");
 		
 	}
 	
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testInventarios(){
+	public void testInventarios() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Inventarios***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/in-proyeccion-actual");//Actual
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Inventarios"); 
+			//Esta prueba puede fallar si se arregla la del label que está abajo
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/in-proyeccion-budget");//Budget
-		
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Inventarios - Budget");
 	}
 	
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testMaestros(){
+	public void testInventariosActualLabels() throws InterruptedException{
+		
+		// Dentro de la sección de /*********Inventarios***********/ pruebo todos sus enlaces:
+		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/in-proyeccion-actual");//Actual
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Inventarios - Actual");
+
+	}
+	
+	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
+	public void testMaestros() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Maestros***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-productos-sku");//Productos
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Maestro de productos");
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-materiales-bom");//Materiales
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Maestro de materiales");
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/ve-alicuotas-iibb");//Alicuotas IIBB
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Maestro de Alicuotas IIBB");
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-canales");//Canales
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Maestro de Canales");
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-clientes");//Clientes
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Maestro de Clientes");
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-codigo-postal");//Cod postales
-		
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Maestro de Códigos Postales");
 	}
 		
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testCostos(){
+	public void testCostos() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Costos***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/in-escenarios-actual");//Actual
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Costos - Actual");
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/in-escenarios-sancor");//STD Sancor
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Escenarios - STD Sancor");
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/in-escenarios-budget");//Budget
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Costos - Budget");	
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/in-escenarios-prj-q1");//PRJ Q1
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Costos - Prj Q1");
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/in-escenarios-prj-q2");//PRJ Q2
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Costos - PRJ Q2");
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/in-escenarios-prj-q3");//PRJ Q3
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Costos - PRJ Q3");
 		
 	}
 	
 	
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testDrivers(){
+	public void testDrivers() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Drivers***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-drivers");//Drivers
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Drivers");
 		
 	}
 	
 	
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testPresentaciones(){
+	public void testPresentaciones() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Presentaciones***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-presentaciones");//Presentaciones
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Presentaciones");
 		
 	}
 	
 	
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testBrandCodes(){
+	public void testBrandCodes() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Brand Codes***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-brand-codes");//Brand Codes
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Brand Codes");
 		
 	}
 	
 	
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testCategorias(){
+	public void testCategoriasLabel() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Categorias***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-categorias");//Categorias
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Categorías");
 		
 	}
 	
 	
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testMgnClass(){
+	public void testCategorias() throws InterruptedException{
+		
+		// Dentro de la sección de /*********Categorias***********/ pruebo todos sus enlaces:
+		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-categorias");//Categorias
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Categorias");
+		
+	}
+	
+	
+	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
+	public void testMgnClass() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Mgn Class***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-mgn-class-i");//Mgn Class I
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Mgn class I");
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-mgn-class-i-i");//Mgn Class II
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Mgn class II");
 		
 	}
 	
 	
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testFamiliaDeProductos(){
+	public void testFamiliaDeProductos() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Familia de productos***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-familia-productos");//Familia de productos
-		
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Familia de productos");
 	}
 	
 	
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testConceptoDeItems(){
+	public void testConceptoDeItems() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Concepto de items***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-concepto-items");//Concepto de items
-		
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Concepto de items");
 	}	
 	
 	
 	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
-	public void testMexicoCategorias(){
+	public void testMexicoCategorias() throws InterruptedException{
 		
 		// Dentro de la sección de /*********Mexico categorias***********/ pruebo todos sus enlaces:
 		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-mexico-categorias");//Mexico categorias
-		
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "Mexico categorias");
+
 	}	
+
+	@Test(dependsOnMethods={"testLoginUserValidoAdmin"})
+	public void testMexicoCategoriasLabel() throws InterruptedException{
+		
+		// Dentro de la sección de /*********Mexico categorias***********/ pruebo todos sus enlaces:
+		driver.navigate().to("http://192.168.1.21:8082/mdj_test/web/mt-mexico-categorias");//Mexico categorias
+			Thread.sleep(5000);
+			Assert.assertEquals((driver.findElement(By.tagName("h1")).getText()), "México categorías");
+
+	}	
+	
 	
 		
 	
